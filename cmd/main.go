d3f8a1546ab932ac7f77965b1cc19936ce72193c
steodhiambo <stephenoginga6@gmail.com>
2024-09-01 14:51:26 +0300

    feat(cmd): add entry point for the application

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"groupie-tracker/api"
+	"log"
+	"net/http"
+	"text/template"
+)
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the template file
+	t, err := template.ParseFiles("templates/homepage.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+
+	// Execute the template with no data
+	if err := t.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", homepageHandler) 
+    http.HandleFunc("/artists", api.ArtistsHandler)
+    http.HandleFunc("/dates", api.DatesHandler)
+    http.HandleFunc("/locations", api.LocationsHandler)
+    http.HandleFunc("/relations", api.RelationsHandler)
+    
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("templates"))))
+	log.Println("Server running on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
