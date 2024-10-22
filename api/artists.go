5bbf320e5b184544158099f3a72394c1cf648619
oumouma <garveyshah1595@gmail.com>
2024-10-22 10:03:53 +0300

    [Refactor] - Refactor to allow for handling single artist page

--- api/artists.go
+++ api/artists.go
@@ -3,6 +3,7 @@ package api
 import (
 	// "encoding/json"
 	// "html/template"
+	"strconv"
 	"net/http"
 	"strings"
 )
@@ -32,16 +33,36 @@ func (h *Handler) ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	searchQuery := r.URL.Query().Get("search")
+	// Get artist ID from thr URL path (if any)
+	urlPath := strings.TrimPrefix(r.URL.Path, "/artist/")
+	artistID, err := strconv.Atoi(urlPath) // consvert to an interger if it is a number
 
+	// URL to fetch all artists
 	url := "https://groupietrackers.herokuapp.com/api/artists"
 	data := []Artist{}
+
 
 	if _, err := h.FetchData(url, &data); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
+	// Check if we are fetching a specific artist by ID
+	if err == nil && artistID > 0 {
+		// Loop through the artist to find the one with the matching ID
+		for _, artist := range data {
+			if artist.ID == artistID {
+				h.RenderTemplate(w, "artists.html", artist) // Render single artist template
+				return
+			}
+		}
+		// iF artist not found, return a 404
+		http.Error(w, "Artist Not Found", http.StatusNotFound)
+		return
+	}
+
+	// If no specific artist is required, display the homepage with all artists
+	searchQuery := r.URL.Query().Get("search")
 	if searchQuery != "" {
 		filtered := []Artist{}
 		for _, artist := range data {
@@ -51,5 +72,5 @@ func (h *Handler) ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		data = filtered
 	}
-	h.RenderTemplate(w, "artists.html", data)
+	h.RenderTemplate(w, "homepage.html", data) // Render homeage with all artists
 }
