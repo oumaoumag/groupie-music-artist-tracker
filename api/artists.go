a89524e0331eb39bda7f09dadd793f43934fc53b
oumouma <garveyshah1595@gmail.com>
2024-10-27 06:59:00 +0300

    uncommented logs

--- api/artists.go
+++ api/artists.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
@@ -32,8 +33,14 @@ func (h *Handler) ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Get artist ID from the URL path (if any)
-	urlPath := strings.TrimPrefix(r.URL.Path, "/artists/")
+	urlPath := strings.TrimPrefix(r.URL.Path, "/artist/")
+
+	log.Printf("r.URL.Path -> %v\n", r.URL.Path)
+	log.Printf("urlPath -> %v\n", urlPath)
+
 	artistID, err := strconv.Atoi(urlPath) // Convert to integer if a number
+
+	log.Printf("ArtistId - %d\n", artistID)
 
 	// URL to fetch all artistss
 	url := "https://groupietrackers.herokuapp.com/api/artists"
@@ -45,11 +52,15 @@ func (h *Handler) ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	log.Printf("Fetched artist data: %+v\n", data)
+
 	// Check if we're fetching a specific artist by ID
 	if err == nil && artistID > 0 {
-		// Find the artist with the matching ID
 		for _, artist := range data {
 			if artist.ID == artistID {
+
+				log.Printf("Rendering artist for ID: %d\n", artistID)
+
 				h.RenderTemplate(w, "artist.html", artist) // Render single artist template
 				return
 			}
@@ -78,10 +89,15 @@ func (h *Handler) ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 
 // homepageHandler: handles requests to the homepagae of the website
 func (h *Handler) HomepageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request for homepage")
+	
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	// Log rendering attempt
+	log.Println("Rendering HomePage - ")
 
 	// URL to fetch all artistss
 	url := "https://groupietrackers.herokuapp.com/api/artists"
@@ -95,4 +111,5 @@ func (h *Handler) HomepageHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Render the homepage with all artists (or filtered results)
 	h.RenderTemplate(w, "homepage.html", data)
+	log.Println("Finished handling request for homepage")
 }
