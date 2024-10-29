4f9bf32cdfdfc13ac5cabdb319a21ed87f48249f
oumouma <garveyshah1595@gmail.com>
2024-10-29 19:03:18 +0300

    [fix] - dates fetching and rendering

--- api/dates.go
+++ api/dates.go
@@ -4,7 +4,10 @@ import (
 	// "encoding/json"
 	// "html/template"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Date struct {
@@ -22,17 +25,60 @@ func DatesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Get and split the URL path
+	path := r.URL.Path
+	parts := strings.Split(path, "/")
+	log.Printf("parts of split dates URL -> %v", parts)
+
+	if len(parts) < 3 {
+		http.Error(w, "Artist ID not found in URL", http.StatusBadRequest)
+		return
+	}
+
+	artistID, err := strconv.Atoi(parts[3])
+	if err != nil {
+		http.Error(w, "Invalid artist ID", http.StatusBadRequest)
+		return
+	}
+
 	url := "https://groupietrackers.herokuapp.com/api/dates"
 	data, err := FetchData(url, &DatesAPIResponse{})
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	RenderTemplate(w, "dates.html", data)
+
+	log.Println("Data fetch successful")
+
+	// TYpe assertion to convert data to *LocationsAPIResponse
+	apiResponse, ok := data.(*DatesAPIResponse)
+	if !ok {
+		http.Error(w, "Invalid data format", http.StatusInternalServerError)
+		return
+	}
+
+	var artistData Date
+	// Filter data for the specific artist
+	for _, artist := range apiResponse.Index {
+		if artist.ID == artistID {
+			artistData = artist
+			break
+		}
+	}
+
+	// If not artist data is found
+	if artistData.ID == 0 {
+		http.Error(w, "Artist not found", http.StatusNotFound)
+		return
+	}
+
+	RenderTemplate(w, "dates.html", artistData)
+	log.Println("Finished rendering Dates")
+
 }
 
 // This function compares the lengths of the Locations and Dates for the artist with the same ID
-func  CompareArtistInfo(w http.ResponseWriter, r *http.Request) {
+func CompareArtistInfo(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
@@ -41,7 +87,7 @@ func  CompareArtistInfo(w http.ResponseWriter, r *http.Request) {
 	// Fetch Locations data
 	locationsURL := "https://groupietrackers.herokuapp.com/api/locations"
 	var locationsResponse LocationsAPIResponse
-	_,err := FetchData(locationsURL, &locationsResponse)
+	_, err := FetchData(locationsURL, &locationsResponse)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
